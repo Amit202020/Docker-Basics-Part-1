e437fad9fb463a739e3c743d4600b56201b8e2ef
Amitava <amitavagc@gmail.com>
2021-10-04 18:16:12 +0530

    class free code

---
+++ java spring boot app/helloworld/src/main/java/com/example/demo/HelloworldApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+	
+	
+	@RequestMapping(value="/welcome", method=RequestMethod.GET)
+	public String getHello() {
+		return "welcome to spring boot";
+	}
+	
+}
